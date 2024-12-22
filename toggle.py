8bbfe3da7dd581ddd386638e10ac90ff32baa46d
BopimoIsPeak <hibopimo@proton.me>
2024-12-22 10:08:22 -0600

    Update toggle.py

--- toggle.py
+++ toggle.py
@@ -1,6 +1,10 @@
 print("Note: In 1.1.0, Bopimo will switch to OpenGL.")
 path = input("Path to Bopimo (Client): ")
-f = open(path + "override.cfg", "x")
+print("Toggling OpenGL in " + path)
+input("Is that okay?")
+f = open(path + "\override.cfg", "x")
+print("Created override.cfg")
 f.write("""[rendering]\n
 renderer/rendering_method="gl_compatibility""")
+print("Toggled OpenGL")
 print("OpenGL should be enabled!")
